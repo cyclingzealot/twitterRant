ae42591df7a69e57cb10d0708e0c9f643fcb5b48
Julien Lamarche <julien.lamarche@gmail.com>
2016-10-08 11:54:09 -0400

    Untested script for treating new lines

--- main.rb
+++ main.rb
@@ -29,40 +29,52 @@ tweet = prefix + ' '
 tweetNum = 1
 startNextTweet = FALSE
 sentenceContinue = FALSE
+pushCurrentTweet = FALSE
 words = text.split(' ')
 tweets = []
 words.each_with_index { |word, i|
 
     nextLength = "#{tweet} #{word} #{tweetNum}/nn #{suffix}".length
 
-    if nextLength >= charsLeft
+    if  /\n\s*\n/.match(word)
+        ## We have one or more new lines
+        startNextTweet  = FALSE
+        sentenceContinue = FALSE
+        pushCurrentTweet = TRUE
+    elsif nextLength >= charsLeft
         ## We are beyond the space alloted for the size of the tweet
         startNextTweet = TRUE
         sentenceContinue = TRUE
+        pushCurrentTweet = TRUE
     elsif tweet.length >= charsLeft*finishSentenceAt and ['!', '?', '.'].member?(tweet[-1, 1])
         ## We have reached the end of the sentence within the
         startNextTweet = TRUE
         sentenceContinue = FALSE
+        pushCurrentTweet = TRUE
     elsif i == words.size - 1
         ## We are at the last word of the entire text
         tweets.push("#{tweet} #{word} #{tweetNum}/#{tweets.size+1} #{suffix}")
         startNextTweet = FALSE
+        pushCurrentTweet = FALSE
     else
         ## We still got space, keep building the tweet,
         tweet += word + ' '
         startNextTweet = FALSE
         sentenceContinue = FALSE
+        pushCurrentTweet = FALSE
     end
 
-    if startNextTweet
-        sentenceCutDots = ''
-        sentenceCutDots = '..' if sentenceContinue
+    sentenceCutDots = ''
+    sentenceCutDots = '..' if sentenceContinue
 
-        # Generate and save the current tweet
+    # Generate and save the current tweet
+    if pushCurrentTweet
         tweets.push("#{tweet} #{tweetNum}/n #{suffix}")
         tweetNum += 1
+    end
 
-        # Start the next tweet with the current word
+    # Start the next tweet with the current word
+    if startNextTweet
         tweet = "#{prefix} #{sentenceCutDots}#{word} "
     end
 
